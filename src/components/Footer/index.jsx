b5b1986a096ec1c3ef3c8a6ac3a3721d821ac1ee
Polzocr <micro.cpt.62@outlook.fr>
2023-04-05 18:49:41 +0200

    header ok

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,41 @@
 export default function Footer() {
     return (
-        <div>FOOTER</div>
+        <footer className="border-top ">
+            <div className="container py-5">
+                <div className="row gy-4 align-items-center">
+                    <div className="col-12 col-md-4">
+                        <a className="navbar-brand text-dark text-uppercase fw-bold" href="#">
+                            <span className="bg-primary bg-gradient p-1 rounded-3 text-light">HR</span> net
+                        </a>
+                    </div>
+                    <div className="col-12 col-md-4 text-md-center">
+                        <ul className="list-unstyled mb-0">
+                            <li>
+                                <a href="#" className="text-decoration-none text-dark">Mentions légales</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-12 col-md-4 text-md-end">
+                        <ul className="list-inline mb-0">
+                            <li className="list-inline-item">
+                                <a href="#" className="text-decoration-none" title="LinkedIn" >
+                                    <i className="fab fa-linkedin fa-2x"></i>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="#" className="text-decoration-none" title="Instagram">
+                                    <i className="fab fa-instagram-square fa-2x"></i>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="#" className="text-decoration-none" title="Twitter">
+                                    <i className="fab fa-twitter-square fa-2x"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Header(){
     return (
-        <div>HEADER</div>
+        <header>
+            <Navbar expand="lg" variant="dark" bg="dark">
+                <Container>
+                    <Navbar.Brand >HRnet</Navbar.Brand>
+                    <Nav className="mr-auto" variant="pills" defaultActiveKey="/">
+                        <Nav.Item>
+                            <NavLink to='/' className='nav-link'>Accueil</NavLink>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <NavLink to='/employee' className='nav-link'>Employés</NavLink>
+                        </Nav.Item>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
