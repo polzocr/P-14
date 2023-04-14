bcab9913ddd65c1b295363bd49991e73ed9779a9
Polzocr <micro.cpt.62@outlook.fr>
2023-04-14 11:12:24 +0200

    ajout proptypes

--- src/components/Selection/index.jsx
+++ src/components/Selection/index.jsx
@@ -1,9 +1,10 @@
 import './index.css'
+import PropTypes from 'prop-types'
 
-export default function Selection({ elements, title, name, onChange }){
+export default function Selection({ title, name, elements, onChange }){
     return (
         <>
-            <select name={name} onChange={onChange} aria-label={`Choisir ${name}`} className="select-plug">
+            <select name={name} onChange={onChange} aria-label={`Choose ${name}`} className="select-plug">
                 <option>{title}</option>
                 {elements.map((element, index) => (
                     <option value={element} key={`${element}-${index}`}>{element}</option>
@@ -11,4 +12,20 @@ export default function Selection({ elements, title, name, onChange }){
             </select>
         </>
     )
+}
+
+Selection.propTypes = {
+    title: PropTypes.string,
+    name: PropTypes.string,
+    elements: PropTypes.arrayOf(PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.bool
+    ])).isRequired,
+    onChange: PropTypes.func
+}
+
+Selection.defaultProps = {
+    title: 'Choose an option',
+    name: 'selected_value',
 }
