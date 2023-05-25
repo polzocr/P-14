5107f430b5e1f2d93c4e6f953e16017a65d3cdd3
Polzocr <micro.cpt.62@outlook.fr>
2023-05-25 20:30:32 +0200

    few changes

--- src/components/Formulaire/index.jsx
+++ src/components/Formulaire/index.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import { saving } from '../../features/employees';
 import {useDispatch} from 'react-redux'
 import MyModal from '../MyModal';
-import Selection from '../Selection';
+// import Selection from "@polzocr/react-selectator"
 import {departments} from '../../data/departments'
 import {states} from '../../data/states';
 import { saveStorageEmployee } from '../../services/storage'
@@ -107,7 +107,7 @@ export default function Formulaire(){
                 <Row>
                     <Col md='6'>
                         <Form.Group className="mb-3" controlId="department" >
-                            <Selection elements={states} title='Etats' name='state' onChange={handleChange} />
+                            {/* <Selection elements={states} title='Etats' name='state' onChange={handleChange} /> */}
                         </Form.Group>
                     </Col>
                     <Col md='6'>
@@ -119,7 +119,7 @@ export default function Formulaire(){
                 <Row>
                     <Col>
                         <Form.Group className="mb-3" controlId="state" >
-                            <Selection elements={departments} title='Département' name='department' onChange={handleChange} />
+                            {/* <Selection elements={departments} title='Département' name='department' onChange={handleChange} /> */}
                         </Form.Group>
                     </Col>
                 </Row>
